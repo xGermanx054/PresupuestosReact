dca2f16dc41383368189281753f82cc6dc9b457c
xGermanx054 <tu_german@hotmail.com>
2019-05-23 15:12:10 -0500

    Proyecto Finalizado V1

---
+++ precache-manifest.9c2bef1102044d8e3ebeff5ec09cbb84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cda74c9b20181e28e4d6e3b16f8892e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87e25ac32adefa0e302e",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "92625e8f421b478da1a1",
+    "url": "/static/js/2.5ca6e9a6.chunk.js"
+  },
+  {
+    "revision": "87e25ac32adefa0e302e",
+    "url": "/static/js/main.a86177bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
